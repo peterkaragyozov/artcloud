f3ef65b3342bd52498d255d49ff1d94003362684
peterkaragyozov <pepitoitalianeca@yahoo.com>
2020-12-11 16:42:36 +0200

    Added CBV for User Profile

--- artcloud/accounts/urls.py
+++ artcloud/accounts/urls.py
@@ -1,13 +1,14 @@
-from django.contrib.auth.views import LoginView
 from django.urls import path, include
 
-from artcloud.accounts.views import user_profile, signup_user, signout_user
+from artcloud.accounts.views import SignUpView, SignOutView, SignInView, UserProfileView
 
 urlpatterns = (
-    # path('signin/', LoginView.as_view(template_name='registration/login.html'), name='signin user', ),
     path('', include('django.contrib.auth.urls')),
-    path('profile/', user_profile, name='current user profile'),
-    path('profile/<int:pk>/', user_profile, name='user profile'),
-    path('signup/', signup_user, name='signup user'),
-    path('signout/', signout_user, name='signout user'),
+    path('profile/', UserProfileView.as_view(), name='current user profile'),
+    path('profile/<int:pk>/', UserProfileView.as_view(), name='user profile'),
+    path('signin/', SignInView.as_view(), name='signin user'),
+    path('signup/', SignUpView.as_view(), name='signup user'),
+    path('signout/', SignOutView.as_view(), name='signout user'),
 )
+
+from .receivers import *
--- artcloud/accounts/views.py
+++ artcloud/accounts/views.py
@@ -1,58 +1,49 @@
-from django.contrib.auth import login, logout
+from django.contrib.auth import login
+from django.contrib.auth import views as auth_views
 from django.contrib.auth.models import User
-from django.shortcuts import render, redirect
+
+from django.urls import reverse_lazy
+from django.views import generic as views
 
 from artcloud.accounts.forms import SignUpForm, UserProfileForm
 from artcloud.accounts.models import UserProfile
 
 
-def user_profile(request, pk=None):
-    user = request.user if pk is None else User.objects.get(pk=pk)
-    if request.method == 'GET':
-        context = {
-            'profile_user': user,
-            'profile': user.userprofile,
-            'arts': user.userprofile.art_set.all(),
-            'form': UserProfileForm(),
-        }
+class UserProfileView(views.UpdateView):
+    template_name = 'accounts/user_profile.html'
+    form_class = UserProfileForm
+    model = UserProfile
+    success_url = reverse_lazy('current user profile')
 
-        return render(request, 'accounts/user_profile.html', context)
-    else:
-        form = UserProfileForm(request.POST, request.FILES, instance=user.userprofile)
-        if form.is_valid():
-            form.save()
-            return redirect('current user profile')
+    def get_object(self, queryset=None):
+        pk = self.kwargs.get('pk', None)
+        user = self.request.user if pk is None else User.objects.get(pk=pk)
+        return user.userprofile
 
-        return redirect('current user profile')
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
 
+        context['profile_user'] = self.get_object().user
+        context['arts'] = self.get_object().art_set.all()
 
-def signup_user(request):
-    if request.method == 'GET':
-        context = {
-            'form': SignUpForm(),
-        }
-
-        return render(request, 'accounts/signup.html', context)
-    else:
-        form = SignUpForm(request.POST)
-
-        if form.is_valid():
-            user = form.save()
-            profile = UserProfile(
-                user=user,
-            )
-            profile.save()
-
-            login(request, user)
-            return redirect('current user profile')
-
-        context = {
-            'form': form,
-        }
-
-        return render(request, 'accounts/signup.html', context)
+        return context
 
 
-def signout_user(request):
-    logout(request)
-    return redirect('index')
+class SignInView(auth_views.LoginView):
+    template_name = 'accounts/signin.html'
+
+
+class SignUpView(views.CreateView):
+    template_name = 'accounts/signup.html'
+    form_class = SignUpForm
+    success_url = reverse_lazy('current user profile')
+
+    def form_valid(self, form):
+        valid = super().form_valid(form)
+        user = form.save()
+        login(self.request, user)
+        return valid
+
+
+class SignOutView(auth_views.LogoutView):
+    next_page = reverse_lazy('index')
